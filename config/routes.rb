ae608d85b548e6356d534702f3aeabaca68894e0
Joleana Shurley <joleanashurley@gmail.com>
2021-02-16 13:57:25 +0100

    read all tasks and read ONE record from tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # read
+  get 'tasks', to: 'tasks#index'
+
+    # create ONE
+
+    # submit form and create the new ONE
+
+  # read ONE
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+    # update
+
+    # submit form and update record for ONE
+
+    # delete ONE record
+
 end
