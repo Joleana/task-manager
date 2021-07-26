dc19faa7e662117bcd0a4a8af8367389e55578b0
Joleana Shurley <joleanashurley@gmail.com>
2021-07-26 23:46:50 +0200

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-15.times do
- Task.create(
-  title: Faker::Lorem.word,
-  details: Faker::Lorem.sentence
-  )
-end
+# 15.times do
+#  Task.create(
+#   title: Faker::Lorem.word,
+#   details: Faker::Lorem.sentence
+#   )
+# end
+Task.destroy_all
+
+Task.create!([{
+  title: "Laundry",
+  details: "Make sure they are separated 🤗"
+  completed: true
+},
+{
+  title: "Groceries",
+  details: "Fruits & veggies for delicious summer treats 🍑🍆"
+  completed: false
+},
+{
+  title: "Cancel gym membership",
+  details: "Don't pay 100 eur a month when you can roller-disco for free  🥳"
+  completed: false
+},
+{
+  title: "Sweep",
+  details: "It's a pain but someone's gotta do it 😅"
+  completed: true
+},
+{
+  title: "Studying",
+  details: "Remember to get your 4 good hours in 🤓"
+  completed: false
+}])
+
+p "Created #{Task.count} tasks"
+
